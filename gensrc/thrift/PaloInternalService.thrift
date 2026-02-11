b13849caee15a9e1b1dd877e574e3ed13a500cc3
xylaaaaa <2392805527@qq.com>
2026-02-11 16:13:59 +0800

    feat(paimon): integrate paimon-cpp reader in BE with FE/thrift toggle

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -434,8 +434,10 @@ struct TQueryOptions {
 
   195: optional bool enable_left_semi_direct_return_opt;
 
-  200: optional bool enable_adjust_conjunct_order_by_cost;
+  // Use paimon-cpp to read Paimon splits on BE
+  196: optional bool enable_paimon_cpp_reader = false;
 
+  200: optional bool enable_adjust_conjunct_order_by_cost;
   // For cloud, to control if the content would be written into file cache
   // In write path, to control if the content would be written into file cache.
   // In read path, read from file cache or remote storage when execute query.
