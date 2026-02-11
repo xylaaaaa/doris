243a5db8384e39100ff59ba040365fb28e2b859d
xylaaaaa <2392805527@qq.com>
2026-02-11 16:12:10 +0800

    fix(thrift): avoid TQueryOptions field id collision for paimon cpp reader

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -429,7 +429,7 @@ struct TQueryOptions {
   195: optional bool enable_left_semi_direct_return_opt;
 
   // Use paimon-cpp to read Paimon splits on BE
-  185: optional bool enable_paimon_cpp_reader = false;
+  196: optional bool enable_paimon_cpp_reader = false;
   // For cloud, to control if the content would be written into file cache
   // In write path, to control if the content would be written into file cache.
   // In read path, read from file cache or remote storage when execute query.
